3ddf0e15b50864ed13fbfff182d1e5f661c57423
brandon1804 <18044455@rp.edu.sg>
2021-06-03 10:05:03 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/id18044455/democooldrawer/BioFragment.java
@@ -0,0 +1,20 @@
+package sg.edu.rp.id18044455.democooldrawer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BioFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bio, container, false);
+    }
+}
---
+++ app/src/main/java/sg/edu/rp/id18044455/democooldrawer/VaccinationFragment.java
@@ -0,0 +1,21 @@
+package sg.edu.rp.id18044455.democooldrawer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class VaccinationFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_vaccination, container, false);
+    }
+}
